a77559dd437ef4ebdae6913855f841fb4b8b1aca
Rami <ramilejmi@gmail.com>
2018-10-24 23:14:52 +0300

    yritys

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -28,17 +28,39 @@ class App extends React.Component {
  
         return (
             <div>
-                <button onClick={this.hyva}>hyva</button>
-                <div>{this.state.hyva}</div>
-                <button onClick={this.neutraali}>neutraali</button>
-                <div>{this.state.neutraali}</div>
-                <button onClick={this.paha}>paha</button>
-                <div>{this.state.paha}</div>
+                <Button nimi= 'hyvä' funktio={this.hyva} tyyppi={this.state.hyva}/>
+                <Button nimi = 'neutraali' funktio={this.neutraali} tyyppi={this.state.neutraali}/>
+                <Button nimi = 'paha' funktio={this.paha} tyyppi={this.state.paha}/>
                 <h1>keskiarvo: {keskiarvo ? keskiarvo : 0}</h1>
-                <h1>prosentti: {prosentti ? prosentti : 0}%</h1>
+                <h1>positiivisia: {prosentti ? prosentti : 0}%</h1>
+                <Statistic props={keskiarvo} />
             </div>
         )
+    }
 }
+const Statistics = () => {
+    return (
+        <div>  
+        </div>
+    )
+}
+const Statistic = (props) => {
+    console.log(props.keskiarvo)
+    return (
+        <div>
+            <h1>{props ? 'kesk' : 'eikesk'} </h1>
+        </div>
+    )
+}
+
+const Button = ({nimi, tyyppi, funktio}) => {
+    console.log('nimi', nimi)
+    return (
+        <div>
+            <button onClick={funktio}>{nimi}</button>
+            <h1>{tyyppi}</h1>
+        </div>
+    )
 }
 ReactDOM.render(<App />, document.getElementById('root'));
 
