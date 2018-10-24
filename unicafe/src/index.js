ff97c43e3cab8e82c783184cb7e7aeb57d9905f4
Rami <ramilejmi@gmail.com>
2018-10-24 20:06:59 +0300

    1.6

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import './index.css';
 
+class App extends React.Component {
+    state = {
+        hyva: 0,
+        neutraali: 0,
+        paha: 0,
+    }
+    hyva = () => {
+        this.setState({hyva: this.state.hyva + 1})
+    }
+    neutraali = () => {
+        this.setState({neutraali: this.state.neutraali + 1})
+    }
+    paha = () => {
+        this.setState({paha: this.state.paha + 1})
+    }
+    render(){   
+        return (
+            <div>
+                <button className="span4 text-right" onClick={this.hyva}>hyva</button>
+                <div>{this.state.hyva}</div>
+                <button onClick={this.neutraali}>neutraali</button>
+                <div>{this.state.neutraali}</div>
+                <button onClick={this.paha}>paha</button>
+                <div>{this.state.paha}</div>
+            </div>
+        )
+}
+}
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
