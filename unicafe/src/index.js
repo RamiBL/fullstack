4f88f87e5e9e35924eee172f6d622e41f15d8081
Rami <ramilejmi@gmail.com>
2018-10-24 20:39:48 +0300

    1.7

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './index.css';
+import './index.css';
 
 class App extends React.Component {
     state = {
         hyva: 0,
         neutraali: 0,
         paha: 0,
+        kaikki: 0
     }
     hyva = () => {
-        this.setState({hyva: this.state.hyva + 1})
+        this.setState({hyva: this.state.hyva + 1,
+                       kaikki: this.state.kaikki + 1})
     }
     neutraali = () => {
-        this.setState({neutraali: this.state.neutraali + 1})
+        this.setState({neutraali: this.state.neutraali + 1,
+                       kaikki: this.state.kaikki + 1})
     }
     paha = () => {
-        this.setState({paha: this.state.paha + 1})
+        this.setState({paha: this.state.paha + 1,
+                       kaikki: this.state.kaikki + 1})
     }
-    render(){   
+    
+    render(){  
+        const keskiarvo = (this.state.hyva - this.state.paha)/this.state.kaikki
+        const prosentti = (this.state.hyva / this.state.kaikki) * 100
+ 
         return (
             <div>
                 <button onClick={this.hyva}>hyva</button>
@@ -26,6 +34,8 @@ class App extends React.Component {
                 <div>{this.state.neutraali}</div>
                 <button onClick={this.paha}>paha</button>
                 <div>{this.state.paha}</div>
+                <h1>keskiarvo: {keskiarvo ? keskiarvo : 0}</h1>
+                <h1>prosentti: {prosentti ? prosentti : 0}%</h1>
             </div>
         )
 }
